5ef63d5c7930d697a4627a54e33bd86b6052c59d
Niklas Heer <niklas.heer@gmail.com>
2016-09-23 12:05:17 +0200

    added search command for arch

--- zsh/arch.zsh
+++ zsh/arch.zsh
@@ -8,3 +8,4 @@ alias pacman-clean="sudo pacman -Sc && sudo pacman-optimize; yaourt -Syua"
 # Software Management
 in() { yaourt --noconfirm -S "$@"; }
 up() { yaourt --noconfirm --aur -Syu; }
+se() { yaourt --noconfirm "$1"; }
