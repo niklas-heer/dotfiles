c84847980d58f4d0c7b04201732672f9f80ddebe
Niklas Heer <niklas.heer@gmail.com>
2016-09-25 17:16:58 +0200

    added command to unlock pacman file

--- zsh/arch.zsh
+++ zsh/arch.zsh
@@ -4,6 +4,7 @@
 
 # Random
 alias pacman-clean="sudo pacman -Sc && sudo pacman-optimize; yaourt -Syua"
+alias pacman-fix="sudo rm /var/lib/pacman/db.lck"
 
 # Software Management
 in() { yaourt --noconfirm -S "$@"; }
