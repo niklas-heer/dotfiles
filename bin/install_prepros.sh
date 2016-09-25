ad7a13084f32a87c741f26d1d67a8260b11d9043
Niklas Heer <niklas.heer@gmail.com>
2016-09-25 18:12:26 +0200

    added prepros installation

--- bin/install_prepros.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Not finished! See: http://discuss.prepros.io/t/install-prepros-on-ubuntu/411/4
-
-# Install Prepros
-echo "Installing Prepros..."
-cd /tmp
-wget http://prepros.io.s3.amazonaws.com/installers/Prepros-Linux-5.9.5-64.zip
-sudo unzip Prepros-Linux-5.9.5-64.zip -d /opt
--- bin/renew_keys_arch.sh
+++ bin/renew_keys_arch.sh
@@ -4,4 +4,4 @@
 sudo mv /etc/pacman.d/gnupg/ /etc/pacman.d/gnupgold
 sudo pacman-key --init
 sudo pacman-key --populate archlinux manjaro
-sudo pacman-key --refresh 
+sudo pacman-key --refresh
