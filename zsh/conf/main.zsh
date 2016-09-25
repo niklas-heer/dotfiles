87ac64c2477f1a1160c9f0d2de428b724780dd8d
Niklas Heer <niklas.heer@gmail.com>
2016-09-25 17:16:36 +0200

    added cheat

--- zsh/conf/main.zsh
+++ zsh/conf/main.zsh
@@ -1,4 +1,8 @@
 export TERM=xterm-256color
 export DEFAULT_USER=nih
 export GOPATH=~/go
-export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin" 
+export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin"
+
+# setting for https://github.com/chrisallenlane/cheat
+export CHEATPATH="$CHEATPATH:$HOME/.dotfiles/cheat"
+export CHEATCOLORS=true
