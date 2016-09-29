f2362c73a2ea84024db1cd87179e4bbc006018bd
Niklas Heer <niklas.heer@gmail.com>
2016-09-29 21:35:26 +0200

    added script to convert sqlite to mysql

---
+++ bin/sqlite3-to-mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# original author: https://github.com/athlite/sqlite3-to-mysql
+
+sed \
+-e '/PRAGMA.*;/ d' \
+-e '/BEGIN TRANSACTION.*/ d' \
+-e '/COMMIT;/ d' \
+-e '/.*sqlite_sequence.*;/d' \
+-e 's/ varchar/ varchar(255)/g' \
+-e 's/"/`/g' \
+-e 's/CREATE TABLE \(`\w\+`\)/DROP TABLE IF EXISTS \1;\nCREATE TABLE \1/' \
+-e 's/\(CREATE TABLE.*\)\(PRIMARY KEY\) \(AUTOINCREMENT\)\(.*\)\();\)/\1AUTO_INCREMENT\4, PRIMARY KEY(id)\5/' \
+-e "s/'t'/1/g" \
+-e "s/'f'/0/g" \
+$1
--- zsh/conf/main.zsh
+++ zsh/conf/main.zsh
@@ -1,7 +1,7 @@
 export TERM=xterm-256color
 export DEFAULT_USER=nih
 export GOPATH=~/go
-export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin"
+export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin"
 
 # setting for https://github.com/chrisallenlane/cheat
 export CHEATPATH="$CHEATPATH:$HOME/.dotfiles/cheat"
