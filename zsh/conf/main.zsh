41365713cae27d649c13435fa08e7cd7bcca4326
Niklas Heer <niklas.heer@gmail.com>
2016-10-06 23:49:34 +0200

    added easy web-development

--- zsh/conf/main.zsh
+++ zsh/conf/main.zsh
@@ -4,7 +4,7 @@ bashcompinit
 export TERM=xterm-256color
 export DEFAULT_USER=nih
 export GOPATH=~/go
-export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin"
+export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin:$HOME/.config/composer/vendor/bin"
 
 # setting for https://github.com/chrisallenlane/cheat
 export CHEATPATH="$CHEATPATH:$HOME/.dotfiles/cheat"
--- zsh/devel.zsh
+++ zsh/devel.zsh
@@ -52,3 +52,60 @@ blog() {
 			;;
 	esac
 }
+
+###################
+# Website
+###################
+
+web() {
+	case "$1" in
+		"new")
+			echo "Creating a new local website...\n"
+
+			# random=`random_string 6`
+			# random="$random.local"
+
+			# echo "\nName? (default: $random)"
+			# read web_name
+
+			# if [[ -z "$web_name" ]]; then
+			# 	web_name="$random"
+			# fi
+
+			# echo "\nDirectory? (default: ${PWD}) "
+			# read web_directory
+
+			# if [[ -z "$web_directory" ]]; then
+			# 	web_directory="${PWD}"
+			# fi
+
+			# echo "\n\nCreating /etc/hosts entry...\n"
+			# echo "127.0.0.1    $web_name    localhost" | sudo tee --append /etc/hosts
+
+			echo "\n\nCreating Caddyfile entry...\n"
+			bash -c "cat <<EOIPFW >> Caddyfile
+:80 {
+	root .
+	tls off
+	fastcgi / 127.0.0.1:9000 php
+}
+EOIPFW"
+			# echo "\n\nVisit your website: http://$web_name (copied to clipboard)"
+			# echo -n "http://$web_name" | pbcopy
+
+			echo "\nStarting caddy..."
+			sudo systemctl restart php-fpm.service
+			# cd $web_directory
+			caddy
+			;;
+		"list")
+			echo "TODO: needs to be implemented"
+			;;
+		"goto")
+			echo "TODO: needs to be implemented"
+			;;
+		*)
+			echo "Function not defined."
+			;;
+	esac
+}
