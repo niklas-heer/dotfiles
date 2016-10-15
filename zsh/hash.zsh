422ff88c1429083241c4a820b9c7b3d368d767d4
Niklas Heer <niklas.heer@gmail.com>
2016-10-15 22:59:10 +0200

    added SHA1 check

--- zsh/hash.zsh
+++ zsh/hash.zsh
@@ -15,11 +15,21 @@ md5()
 # shortcut for sha256 hash
 sha256()
 {
-    md5_raw=`sha256sum $1`
+    sha256_raw=`sha256sum $1`
 
     # cut out the unnecessary stuff
-    md5=${md5_raw:0:64}
-    echo $md5
+    sha256=${sha256_raw:0:64}
+    echo $sha256
+}
+
+# shortcut for sha256 hash
+sha1()
+{
+    sha1_raw=`sha1sum $1`
+
+    # cut out the unnecessary stuff
+    sha1=${sha1_raw:0:40}
+    echo $sha1
 }
 
 # verify md5
@@ -30,9 +40,9 @@ md5_check()
 
 	if [ "$file_md5" = "$check_md5" ]
 	then
-	    echo "[MD5 CHECK] success!"
+	    echo "[MD5 CHECK] File is okay!"
 	else
-		echo "[MD5 CHECK] failed!"
+		echo "[MD5 CHECK] File is corrupted!"
 	fi
 }
 
@@ -44,8 +54,22 @@ sha256_check()
 
 	if [ "$file_sha256" = "$check_sha256" ]
 	then
-	    echo "[SHA256 CHECK] success!"
+	    echo "[SHA256 CHECK] File is okay!"
 	else
-		echo "[SHA256 CHECK] failed!"
+		echo "[SHA256 CHECK] File is corrupted!"
+	fi
+}
+
+# verify sha256
+sha1_check()
+{
+	file_sha1=`sha1 $1`
+	check_sha1=`echo $2`
+
+	if [ "$file_sha1" = "$check_sha1" ]
+	then
+	    echo "[SHA1 CHECK] File is okay!"
+	else
+		echo "[SHA1 CHECK] File is corrupted!"
 	fi
 }
