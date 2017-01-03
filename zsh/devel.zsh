13515b98a7a8515cc17dd87e8487de65299451c6
Niklas Heer <niklas.heer@gmail.com>
2017-01-03 17:19:28 +0100

    fixed glcd command

--- zsh/devel.zsh
+++ zsh/devel.zsh
@@ -11,18 +11,26 @@ gdis() { git stash save --keep-index; git stash drop }
 glcd() {
 	# source: https://gist.github.com/azu/8205567
     if [ ! -n "$1" ]; then
-        echo "Usage: gcd git://example.com/repo.git"
+        echo "Usage: glcd git://example.com/repo.git"
         return;
     fi
-    declare url=$1;
-    declare reponame=$(echo $url | awk -F/ '{print $NF}' | sed -e 's/.git$//');
+
+    url=$1
+
+    if [ ! -n "$2" ]; then
+    	reponame=$(echo $url | awk -F/ '{print $NF}' | sed -e 's/.git$//')
+    else
+    	reponame=$2
+    fi
+
     if [ -d "$reponame" ]; then
-        cd $reponame;
-        return;
+        cd $reponame
+        return
     fi
-    git clone --recursive $url $reponame;
+
+    git clone --recursive $url $reponame
     echo "\033[31m=>\033[0m \033[036m$reponame\033[0m"
-    cd $reponame;
+    cd $reponame
 }
 
 ###################
