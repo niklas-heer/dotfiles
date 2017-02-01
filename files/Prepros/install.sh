62d459eb6c1b304ba7a7aa6b9c8311fa355fdb4a
Niklas Heer <niklas.heer@gmail.com>
2017-02-01 22:20:23 +0100

    cleanup

--- files/Prepros/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# See: http://discuss.prepros.io/t/install-prepros-on-ubuntu/411/4
-
-# Install Prepros
-cd /tmp
-wget http://prepros.io.s3.amazonaws.com/installers/Prepros-Linux-5.10.2-64.zip
-sudo unzip Prepros-Linux-5.10.2-64.zip -d /opt
-sudo cp ~/.dotfiles/files/Prepros/prepros.png /opt/Prepros-linux-64
