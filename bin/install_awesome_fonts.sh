0f949f18889b62121bf2bed5539a317ed1be5983
Niklas Heer <niklas.heer@gmail.com>
2017-03-05 23:47:12 +0100

    added fira and awesome font

---
+++ bin/install_awesome_fonts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ~/.local/share/fonts
+for font in Droid+Sans+Mono+Awesome Inconsolata+Awesome SourceCodePro+Powerline+Awesome+Regular; do
+	wget -O ~/.local/share/fonts/$font.ttf "https://github.com/gabrielelana/awesome-terminal-fonts/blob/patching-strategy/patched/$font.ttf?raw=true";
+done
+fc-cache -f
