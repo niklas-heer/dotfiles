9debf981ee7d81fe5065a790068441ae5bf97b1e
Niklas Heer <me@nheer.io>
2017-03-21 15:22:46 +0100

    cosmetic change

--- zsh/master/main.zsh
+++ zsh/master/main.zsh
@@ -15,10 +15,10 @@ export CHEATCOLORS=true
 fpath=($HOME/.zsh/autocompletion $fpath)
 
 # Load more configuration
-source ~/.zsh/master/antigen.zsh           # Load and configure Antigen
-source ~/.zsh/master/powerlevel9k.zsh      # Configure Powerlevel9k
-source ~/.zsh/master/helpers.zsh           # Load helper functions
-source ~/.dotfiles/.simplepush.conf      # Load Simplepush Key
+source ~/.zsh/master/antigen.zsh            # Load and configure Antigen
+source ~/.zsh/master/powerlevel9k.zsh       # Configure Powerlevel9k
+source ~/.zsh/master/helpers.zsh            # Load helper functions
+source ~/.dotfiles/.simplepush.conf         # Load Simplepush Key
 
 # Aliases and Functions
 source ~/.zsh/general.zsh                   # General Shortcuts
