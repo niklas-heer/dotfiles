dcf80cf1c2e4233b06476e975a48a084def6c84d
Niklas Heer <niklas.heer@gmail.com>
2017-04-09 23:52:07 +0200

    added script to handle pip deps

---
+++ bin/install_pip_deps.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Description: This script installs all pip deps.
+# Author: Niklas Heer (niklas.heer@gmail.com)
+# Version: 1.0.0 (2017-04-09)
+
+main() {
+	pip_version="pip3"
+
+	if [ -f /etc/solus-release ]; then
+		# Solus
+		pip_version="pip"
+	fi
+
+	sudo -H $pip_version install -r $HOME/.dotfiles/pip.conf --upgrade
+}
+
+main
