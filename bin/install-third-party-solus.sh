f9a64e0e1b309afcd64c63ae595f327f4a23c943
Niklas Heer <niklas.heer@gmail.com>
2017-04-15 19:33:27 +0200

    adds support to install third party apps

---
+++ bin/install-third-party-solus.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Description: This script installs third party apps on solus.
+# Author: Niklas Heer (niklas.heer@gmail.com)
+# Version: 1.0.0 (2017-04-15)
+
+main() {
+    cd /tmp
+
+    # Install Skype
+    sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/skype/pspec.xml
+    sudo eopkg it skype*.eopkg;sudo rm *.eopkg
+}
+
+main
