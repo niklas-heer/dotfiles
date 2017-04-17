a7f624fd9898ce2da950db75b393afe47c5a92c4
Niklas Heer <niklas.heer@gmail.com>
2017-04-17 22:02:58 +0200

    adds google-chrome

--- bin/install-third-party-solus.sh
+++ bin/install-third-party-solus.sh
@@ -7,6 +7,10 @@
 main() {
     cd /tmp
 
+    # Install Chrome
+    sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
+    sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
+
     # Install Skype
     sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/skype/pspec.xml
     sudo eopkg it skype*.eopkg;sudo rm *.eopkg
