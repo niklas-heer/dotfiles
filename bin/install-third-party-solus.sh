1ce208e60c9e8a2494c45227cba4b048c0472575
Niklas Heer <niklas.heer@gmail.com>
2017-04-23 18:11:20 +0200

    adds reference to orginal site

--- bin/install-third-party-solus.sh
+++ bin/install-third-party-solus.sh
@@ -7,6 +7,8 @@
 main() {
     cd /tmp
 
+    # see: https://solus-project.com/articles/software/third-party/en/
+
     # Install Chrome
     sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
     sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
