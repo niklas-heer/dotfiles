fdf5362349b66546f0cda2870584ac1c9db26b9e
Niklas Heer <niklas.heer@gmail.com>
2017-05-01 12:28:24 +0200

    converts font scripts to a better style

--- bin/install-font_awesome.sh
+++ bin/install-font_awesome.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+#
+# Description: This script installs awesome fonts.
+# Author: Niklas Heer (niklas.heer@gmail.com)
+# Version: 1.0.0 (2017-05-01)
 
-mkdir -p ~/.local/share/fonts
-for font in Droid+Sans+Mono+Awesome Inconsolata+Awesome SourceCodePro+Powerline+Awesome+Regular; do
-	wget -O ~/.local/share/fonts/$font.ttf "https://github.com/gabrielelana/awesome-terminal-fonts/blob/patching-strategy/patched/$font.ttf?raw=true";
-done
-fc-cache -f
+main() {
+    mkdir -p ~/.local/share/fonts
+    for font in Droid+Sans+Mono+Awesome Inconsolata+Awesome SourceCodePro+Powerline+Awesome+Regular; do
+        wget -O ~/.local/share/fonts/$font.ttf "https://github.com/gabrielelana/awesome-terminal-fonts/blob/patching-strategy/patched/$font.ttf?raw=true";
+    done
+    fc-cache -f
+}
+
+main
--- bin/install-font_fira.sh
+++ bin/install-font_fira.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+#
+# Description: This script installs the FiraCode font.
+# Author: Niklas Heer (niklas.heer@gmail.com)
+# Version: 1.0.0 (2017-05-01)
 
-mkdir -p ~/.local/share/fonts
-for type in Bold Light Medium Regular Retina; do
-	wget -O ~/.local/share/fonts/FiraCode-$type.ttf "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-$type.ttf?raw=true";
-done
-fc-cache -f
+main() {
+    mkdir -p ~/.local/share/fonts
+    for type in Bold Light Medium Regular Retina; do
+        wget -O ~/.local/share/fonts/FiraCode-$type.ttf "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-$type.ttf?raw=true";
+    done
+    fc-cache -f
+}
+
+main
