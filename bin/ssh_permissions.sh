d6ea9bef509e74299a8a85d069a62b591700678e
Niklas Heer <niklas.heer@gmail.com>
2017-05-08 11:01:03 +0200

    fix the coding style to a better one

--- bin/ssh_permissions.sh
+++ bin/ssh_permissions.sh
@@ -1,16 +1,27 @@
-#!/bin/sh
-# see: http://superuser.com/questions/215504/permissions-on-private-key-in-ssh-folder
+#!/usr/bin/env bash
+#
+# Description: This script sets the correct permissions for the ".ssh" folder.
+# Reference: http://superuser.com/questions/215504/permissions-on-private-key-in-ssh-folder
+# Author: Niklas Heer (niklas.heer@gmail.com)
+# Version: 1.0.0 (2017-05-08)
 
-SSH_FOLDER="$HOME/.ssh"
+main() {
+    SSH_FOLDER="$HOME/.ssh"
 
-chmod 700 $SSH_FOLDER
-chmod 600 $SSH_FOLDER/*
-chmod 644 $SSH_FOLDER/*.pub
+    # main permissions
+    chmod 700 $SSH_FOLDER
+    chmod 600 $SSH_FOLDER/*
+    chmod 644 $SSH_FOLDER/*.pub
 
-if [ -f $SSH_FOLDER/known_hosts ]; then
-    chmod 644 $SSH_FOLDER/known_hosts
-fi
+    # known_hosts
+    if [ -f $SSH_FOLDER/known_hosts ]; then
+        chmod 644 $SSH_FOLDER/known_hosts
+    fi
 
-if [ -f $SSH_FOLDER/config ]; then
-    chmod 644 $SSH_FOLDER/config
-fi
+    # config
+    if [ -f $SSH_FOLDER/config ]; then
+        chmod 644 $SSH_FOLDER/config
+    fi
+}
+
+main
