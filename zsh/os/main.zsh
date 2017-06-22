1ffee5d35a06e2f2867a169ed187a8fbab80d7bb
Niklas Heer <niklas.heer@gmail.com>
2017-06-22 09:41:57 +0200

    add support for manjaro

--- zsh/os/main.zsh
+++ zsh/os/main.zsh
@@ -1,6 +1,6 @@
 # This provides commands for the specific OS which is in use.
 
-if [ -f /etc/arch-release ]; then
+if [ -f /etc/arch-release ] || [ -f /etc/manjaro-release ]; then
     # arch/manjaro/antergos
     source ~/.zsh/os/arch.zsh
 elif [ -f /etc/solus-release ]; then
