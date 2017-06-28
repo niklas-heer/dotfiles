29c1cebf874b6fb5f6d7158280b1761152d00e22
Niklas Heer <niklas.heer@gmail.com>
2017-06-28 21:48:19 +0200

    add local pip installs

--- zsh/master/main.zsh
+++ zsh/master/main.zsh
@@ -5,7 +5,7 @@ export TERM=xterm-256color
 export EDITOR=/usr/bin/subl
 export DEFAULT_USER=nih
 export GOPATH=~/go
-export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin:$HOME/.config/composer/vendor/bin"
+export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin:$HOME/.config/composer/vendor/bin:$HOME/.local/bin"
 
 # setting for https://github.com/chrisallenlane/cheat
 export CHEATPATH="$CHEATPATH:$HOME/.dotfiles/cheat"
