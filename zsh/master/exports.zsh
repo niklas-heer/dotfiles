72ab78da3517c8831047a6281e89f7ef0b02bede
Niklas Heer <niklas.heer@gmail.com>
2017-07-13 23:04:37 +0200

    add exports to a single file

---
+++ zsh/master/exports.zsh
@@ -0,0 +1,9 @@
+export TERM=xterm-256color
+export EDITOR=/usr/bin/subl
+export DEFAULT_USER=nih
+export GOPATH=~/go
+export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin:$HOME/.config/composer/vendor/bin:$HOME/.local/bin"
+
+# setting for https://github.com/chrisallenlane/cheat
+export CHEATPATH="$CHEATPATH:$HOME/.dotfiles/cheat"
+export CHEATCOLORS=true
--- zsh/master/main.zsh
+++ zsh/master/main.zsh
@@ -1,15 +1,8 @@
 autoload bashcompinit
 bashcompinit
 
-export TERM=xterm-256color
-export EDITOR=/usr/bin/subl
-export DEFAULT_USER=nih
-export GOPATH=~/go
-export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin:$HOME/.config/composer/vendor/bin:$HOME/.local/bin"
-
-# setting for https://github.com/chrisallenlane/cheat
-export CHEATPATH="$CHEATPATH:$HOME/.dotfiles/cheat"
-export CHEATCOLORS=true
+# Load exports
+source ~/.zsh/master/exports.zsh
 
 # Autocompletion files
 fpath=($HOME/.zsh/autocompletion $fpath)
