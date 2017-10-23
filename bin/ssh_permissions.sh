324f432815570302cf6a003a6a87294d5b8956cf
Niklas Heer <me@nheer.io>
2017-10-23 16:02:09 +0200

    Fix shellcheck errors

--- bin/ssh_permissions.sh
+++ bin/ssh_permissions.sh
@@ -9,18 +9,18 @@ main() {
     SSH_FOLDER="$HOME/.ssh"
 
     # main permissions
-    chmod 700 $SSH_FOLDER
-    chmod 600 $SSH_FOLDER/*
-    chmod 644 $SSH_FOLDER/*.pub
+    chmod 700 "$SSH_FOLDER"
+    chmod 600 "$SSH_FOLDER/*"
+    chmod 644 "$SSH_FOLDER/*.pub"
 
     # known_hosts
-    if [ -f $SSH_FOLDER/known_hosts ]; then
-        chmod 644 $SSH_FOLDER/known_hosts
+    if [ -f "$SSH_FOLDER/known_hosts" ]; then
+        chmod 644 "$SSH_FOLDER/known_hosts"
     fi
 
     # config
-    if [ -f $SSH_FOLDER/config ]; then
-        chmod 644 $SSH_FOLDER/config
+    if [ -f "$SSH_FOLDER/config" ]; then
+        chmod 644 "$SSH_FOLDER/config"
     fi
 }
 
