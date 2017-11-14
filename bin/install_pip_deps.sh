fa84dfe44a9e077436d2a9c8b068498ca991f6c2
Niklas Heer <niklas.heer@gmail.com>
2017-11-14 09:30:44 +0100

    Add macOS support

--- bin/install_pip_deps.sh
+++ bin/install_pip_deps.sh
@@ -2,17 +2,24 @@
 #
 # Description: This script installs all pip deps.
 # Author: Niklas Heer (niklas.heer@gmail.com)
-# Version: 1.0.0 (2017-04-09)
+# Version: 1.1.0 (2017-11-13)
 
 main() {
-    pip_version="pip3"
+	pip_version="pip3"
 
-    if [ -f /etc/solus-release ]; then
-        # Solus
-        pip_version="pip"
-    fi
+	if [[ $(uname) == 'Darwin' ]]; then
+		# MacOS
+		"$pip_version" install -r "$HOME/.dotfiles/pip.conf" --upgrade
+	else
+		# Linux
 
-    sudo -H $pip_version install -r $HOME/.dotfiles/pip.conf --upgrade
+		if [ -f /etc/solus-release ]; then
+			# Solus
+			pip_version="pip"
+		fi
+
+		sudo -H "$pip_version" install -r "$HOME/.dotfiles/pip.conf" --upgrade
+	fi
 }
 
 main
