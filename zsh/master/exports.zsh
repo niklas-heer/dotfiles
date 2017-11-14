9f6290971494d304c541f601678d358e3d48c18e
Niklas Heer <niklas.heer@gmail.com>
2017-11-14 09:33:08 +0100

    Add „/usr/local/bin“ to the $PATH

--- zsh/master/exports.zsh
+++ zsh/master/exports.zsh
@@ -5,8 +5,8 @@
 export TERM=xterm-256color
 export EDITOR=/usr/bin/subl
 export DEFAULT_USER=nih
-export PATH="$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin:$HOME/.config/composer/vendor/bin:$HOME/.local/bin"
 export GOPATH="$HOME/Go"
+export PATH="/usr/local/bin:$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin:$HOME/.composer/vendor/bin:$HOME/.local/bin"
 
 # setting for https://github.com/chrisallenlane/cheat
 export CHEATPATH="$CHEATPATH:$HOME/.dotfiles/cheat"
