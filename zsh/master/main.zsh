396087c155005e19745c970983c7f6fb9cc85dbb
Niklas Heer <niklas.heer@gmail.com>
2017-11-19 23:13:13 +0100

    Refactor sourcing

--- zsh/master/main.zsh
+++ zsh/master/main.zsh
@@ -6,27 +6,25 @@ autoload bashcompinit
 bashcompinit
 
 # Load exports
-source ~/.zsh/master/exports.zsh
+source "$HOME/.zsh/master/exports.zsh"
 
 # Autocompletion files
 fpath=($HOME/.zsh/autocompletion $fpath)
 
 # Load more configuration
-source ~/.zsh/master/antigen.zsh            # Load and configure Antigen
-source ~/.zsh/master/powerlevel9k.zsh       # Configure Powerlevel9k
-source ~/.zsh/master/helpers.zsh            # Load helper functions
+source "$HOME/.zsh/master/antigen.zsh"         # Load and configure Antigen
+source "$HOME/.zsh/master/powerlevel9k.zsh"    # Configure Powerlevel9k
+source "$HOME/.zsh/master/helpers.zsh"         # Load helper functions
 
 # Load Maybe files
-include ~/.dotfiles/.simplepush.conf         # Load Simplepush Key
+include "$HOME/.dotfiles/.simplepush.conf"     # Load Simplepush Key
+include /etc/profile.d/vte.sh                  # VTE support
 include "$HOME/.cargo/env"                     # Load rust environment
 
 # Aliases and Functions
-source ~/.zsh/general.zsh                   # General Shortcuts
-source ~/.zsh/aliases.zsh                   # Aliases
-source_files_in_dir ~/.zsh/dev/*.zsh        # Development Shortcuts
-source ~/.zsh/misc.zsh                      # Miscelaneous shortcuts/tools
-source_files_in_dir ~/.zsh/scripts/*.zsh    # Scripts
-source ~/.zsh/os/main.zsh                   # Special commands for the current os
-
-# VTE support
-include /etc/profile.d/vte.sh
+source "$HOME/.zsh/general.zsh"                # General Shortcuts
+source "$HOME/.zsh/aliases.zsh"                # Aliases
+source_files_in_dir "$HOME"/.zsh/dev/*.zsh     # Development Shortcuts
+source "$HOME/.zsh/misc.zsh"                   # Miscelaneous shortcuts/tools
+source_files_in_dir "$HOME"/.zsh/scripts/*.zsh # Scripts
+source "$HOME/.zsh/os/main.zsh"                # Special commands for the current os
