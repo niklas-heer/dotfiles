ee4ed7f148d949d2ee563cfad60456ea09dfaf32
Niklas Heer <niklas.heer@gmail.com>
2017-11-20 08:42:36 +0100

    Add macOS specific config

--- zsh/os/main.zsh
+++ zsh/os/main.zsh
@@ -3,13 +3,20 @@
 #####################
 
 # This provides commands for the specific OS which is in use.
-if [ -f /etc/arch-release ] || [ -f /etc/manjaro-release ]; then
-    # arch/manjaro/antergos
-    source ~/.zsh/os/arch.zsh
-elif [ -f /etc/solus-release ]; then
-    # Solus
-    source ~/.zsh/os/solus.zsh
-elif [ -f /etc/lsb-release  ]; then
-    # Ubuntu
-    source ~/.zsh/os/ubuntu.zsh
+
+if [[ $(uname) == 'Darwin' ]]; then
+	# MacOS
+	source "$HOME/.zsh/os/mac.zsh"
+else
+	# Linux
+	if [ -f /etc/arch-release ] || [ -f /etc/manjaro-release ]; then
+		# arch/manjaro/antergos
+		source "$HOME/.zsh/os/arch.zsh"
+	elif [ -f /etc/solus-release ]; then
+		# Solus
+		source "$HOME/.zsh/os/solus.zsh"
+	elif [ -f /etc/lsb-release ]; then
+		# Ubuntu
+		source "$HOME/.zsh/os/ubuntu.zsh"
+	fi
 fi
