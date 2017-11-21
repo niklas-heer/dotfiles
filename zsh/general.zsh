64f1763038ab45c46f84f738d97034d9835572c2
Niklas Heer <niklas.heer@gmail.com>
2017-11-21 20:23:41 +0100

    Rename up function to upd

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -49,26 +49,26 @@ e() {
 }
 
 # Go up directory tree X number of directories
-up () {
-    COUNTER="$@";
-    # default $COUNTER to 1 if it isn't already set
-    if [[ -z $COUNTER ]]; then
-        COUNTER=1
-    fi
-    # make sure $COUNTER is a number
-    if [ $COUNTER -eq $COUNTER 2> /dev/null ]; then
-        nwd=`pwd` # Set new working directory (nwd) to current directory
-        # Loop $nwd up directory tree one at a time
-        until [[ $COUNTER -lt 1 ]]; do
-            nwd=`dirname $nwd`
-            let COUNTER-=1
-        done
-        cd $nwd # change directories to the new working directory
-    else
-        # print usage and return error
-        echo "usage: up [NUMBER]"
-        return 1
-    fi
+upd() {
+	COUNTER="$*"
+	# default $COUNTER to 1 if it isn't already set
+	if [[ -z $COUNTER ]]; then
+		COUNTER=1
+	fi
+	# make sure $COUNTER is a number
+	if [ $COUNTER -eq $COUNTER ] 2>/dev/null; then
+		nwd=$(pwd) # Set new working directory (nwd) to current directory
+		# Loop $nwd up directory tree one at a time
+		until [[ $COUNTER -lt 1 ]]; do
+			nwd=$(dirname "$nwd")
+			let COUNTER-=1
+		done
+		cd "$nwd" || exit # change directories to the new working directory
+	else
+		# print usage and return error
+		echo "usage: up [NUMBER]"
+		return 1
+	fi
 }
 
 # ------
