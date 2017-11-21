ed9180a48e852ab57640b12cc5283b9822c723e4
Niklas Heer <niklas.heer@gmail.com>
2017-11-21 20:30:43 +0100

    Refactor to use include instead of source

--- zsh/master/main.zsh
+++ zsh/master/main.zsh
@@ -7,15 +7,15 @@ autoload bashcompinit
 bashcompinit
 
 # Load exports
-source "$HOME/.zsh/master/exports.zsh"
+include "$HOME/.zsh/master/exports.zsh"
 
 # Autocompletion files
 fpath=($HOME/.zsh/autocompletion $fpath)
 
 # Load more configuration
-source "$HOME/.zsh/master/antigen.zsh"         # Load and configure Antigen
-source "$HOME/.zsh/master/powerlevel9k.zsh"    # Configure Powerlevel9k
-source "$HOME/.zsh/master/helpers.zsh"         # Load helper functions
+include "$HOME/.zsh/master/antigen.zsh"        # Load and configure Antigen
+include "$HOME/.zsh/master/powerlevel9k.zsh"   # Configure Powerlevel9k
+include "$HOME/.zsh/master/helpers.zsh"        # Load helper functions
 
 # Load Maybe files
 include "$HOME/.dotfiles/.simplepush.conf"     # Load Simplepush Key
@@ -23,9 +23,9 @@ include /etc/profile.d/vte.sh                  # VTE support
 include "$HOME/.cargo/env"                     # Load rust environment
 
 # Aliases and Functions
-source "$HOME/.zsh/general.zsh"                # General Shortcuts
-source "$HOME/.zsh/aliases.zsh"                # Aliases
+include "$HOME/.zsh/general.zsh"               # General Shortcuts
+include "$HOME/.zsh/aliases.zsh"               # Aliases
 source_files_in_dir "$HOME"/.zsh/dev/*.zsh     # Development Shortcuts
-source "$HOME/.zsh/misc.zsh"                   # Miscelaneous shortcuts/tools
+include "$HOME/.zsh/misc.zsh"                  # Miscelaneous shortcuts/tools
 source_files_in_dir "$HOME"/.zsh/scripts/*.zsh # Scripts
-source "$HOME/.zsh/os/main.zsh"                # Special commands for the current os
+include "$HOME/.zsh/os/main.zsh"               # Special commands for the current os
