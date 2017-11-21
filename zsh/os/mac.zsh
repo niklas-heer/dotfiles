088a21c6aeaff77326ce095fe74d182afc123754
Niklas Heer <niklas.heer@gmail.com>
2017-11-21 19:41:26 +0100

    Add custom mac commands to install stuff

--- zsh/os/mac.zsh
+++ zsh/os/mac.zsh
@@ -1,3 +1,24 @@
 #############
 # macOS
 #############
+
+# Custom Aliases
+alias cask="brew cask"
+
+# Software Management
+# ------------------
+in() {
+	# Install Shortcut (brew)
+	brew install "$@"
+}
+
+cin() {
+	# Install Shortcut (brew cask)
+	cask install "$@"
+}
+
+update() {
+	# Update Shortcut
+	brew update
+	brew upgrade
+}
