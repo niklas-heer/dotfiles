da1cb92adc048414d73ffc9fec669cf33f9daa0f
Niklas Heer <niklas.heer@gmail.com>
2017-12-11 08:45:27 +0100

    Improve installation process

--- bin/fonts/install-font_awesome.sh
+++ bin/fonts/install-font_awesome.sh
@@ -2,14 +2,30 @@
 #
 # Description: This script installs awesome fonts.
 # Author: Niklas Heer (niklas.heer@gmail.com)
-# Version: 1.0.0 (2017-05-01)
+# Version: 1.2.0
 
 main() {
-    mkdir -p "$HOME/.local/share/fonts"
-    for font in Droid+Sans+Mono+Awesome Inconsolata+Awesome SourceCodePro+Powerline+Awesome+Regular; do
-        wget -O "$HOME/.local/share/fonts/$font.ttf" "https://github.com/gabrielelana/awesome-terminal-fonts/blob/patching-strategy/patched/$font.ttf?raw=true";
-    done
-    fc-cache -f
+
+	if [[ $(uname) == 'Darwin' ]]; then
+		# MacOS
+		font_dir="$HOME/Library/Fonts"
+	else
+		# Linux
+		font_dir="$HOME/.local/share/fonts"
+		mkdir -p "$font_dir"
+	fi
+
+	for font in Droid+Sans+Mono+Awesome Inconsolata+Awesome SourceCodePro+Powerline+Awesome+Regular; do
+		wget -O "$font_dir/$font.ttf" "https://github.com/gabrielelana/awesome-terminal-fonts/blob/patching-strategy/patched/$font.ttf?raw=true"
+	done
+
+	# Reset font cache on Linux
+	if command -v fc-cache @>/dev/null; then
+		echo "Resetting font cache, this may take a moment..."
+		fc-cache -f "$font_dir"
+	fi
+
+	echo "Installed all awesome paatched fonts to $font_dir"
 }
 
 main
