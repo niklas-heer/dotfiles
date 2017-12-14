41e497d23dd8b027d2006bbb8974100dffc97790
Niklas Heer <niklas.heer@gmail.com>
2017-12-14 09:17:51 +0100

    Refactor to make installation more versatile

--- bin/fonts/install-font_fira.sh
+++ bin/fonts/install-font_fira.sh
@@ -2,14 +2,31 @@
 #
 # Description: This script installs the FiraCode font.
 # Author: Niklas Heer (niklas.heer@gmail.com)
-# Version: 1.0.0 (2017-05-01)
+# Version: 1.1.0
 
 main() {
-    mkdir -p ~/.local/share/fonts
-    for type in Bold Light Medium Regular Retina; do
-        wget -O ~/.local/share/fonts/FiraCode-$type.ttf "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-$type.ttf?raw=true";
-    done
-    fc-cache -f
+
+	if [[ $(uname) == 'Darwin' ]]; then
+		# MacOS
+		font_dir="$HOME/Library/Fonts"
+	else
+		# Linux
+		font_dir="$HOME/.local/share/fonts"
+		mkdir -p "$font_dir"
+	fi
+
+	for type in Bold Light Medium Regular Retina; do
+		wget -O "$font_dir/FiraCode-$type.ttf"
+		"https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-$type.ttf?raw=true"
+	done
+
+	# Reset font cache on Linux
+	if command -v fc-cache @>/dev/null; then
+		echo "Resetting font cache, this may take a moment..."
+		fc-cache -f "$font_dir"
+	fi
+
+	echo "Installed FiraCode to $font_dir"
 }
 
 main
