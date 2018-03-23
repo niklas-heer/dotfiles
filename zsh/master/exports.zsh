2ddb6ed7db8af43f2c7de3c7394538e92a9b3dc8
Niklas Heer <niklas.heer@gmail.com>
2018-03-23 18:08:29 +0100

    Fix default user

--- zsh/master/exports.zsh
+++ zsh/master/exports.zsh
@@ -5,7 +5,7 @@
 
 export DOTFILES="$HOME/.dotfiles"
 export TERM=xterm-256color
-export DEFAULT_USER=nih
+export DEFAULT_USER=$USER
 export GOPATH="$HOME/Go"
 # shellcheck disable=SC2155
 export PATH="/usr/local/bin:$PATH:$GOPATH/bin:$(ruby -e "print Gem.user_dir")/bin:$HOME/.dotfiles/bin:$HOME/.composer/vendor/bin:$HOME/.local/bin:$HOME/.cargo/bin"
