3f18dcb768125dfc9cbd4f95673ae31de0ecbe3d
Niklas Heer <niklas.heer@gmail.com>
2018-04-20 14:39:25 +0200

    Add virtualenvwrapper bugfix script

---
+++ bin/fix-ubuntu-virtualenvwrapper.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Description: This script fixes the following error:
+# > virtualenvwrapper.sh: There was a problem running the initialization hooks.
+# Author: Niklas Heer (niklas.heer@gmail.com)
+# Version: 1.0.0 (2018-04-20)
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+main() {
+    pushd /usr/local/bin
+    sudo ln -s `which python3` python
+    popd
+
+    echo "The problem should be fixed! Have fun :)"
+}
+
+main
