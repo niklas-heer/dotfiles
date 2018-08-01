08795f3e8a87306d845b2d1fe6e3d94ebbed2090
Niklas Heer <niklas.heer@gmail.com>
2018-08-01 20:30:57 +0200

    Add ability to open the url of a repo

--- zsh/dev/master.zsh
+++ zsh/dev/master.zsh
@@ -19,3 +19,13 @@ alias ecs="exercism submit"
 alias utf8="~/.dotfiles/bin/toUTF8.sh"
 alias fix_caddy="sudo setcap cap_net_bind_service=+ep /usr/sbin/caddy &>/dev/null"
 alias s="subl ."
+
+# Open the url of the curent repo
+op () {
+    proj_dir='Projects'
+    current_dir=$(pwd)
+
+    if [[ $current_dir = *$proj_dir* ]]; then
+        python -m webbrowser "http://${current_dir##*$proj_dir/}"
+    fi
+}
