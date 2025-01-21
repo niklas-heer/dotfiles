798aaa7afa626515a8db43be331dde764b08e39a
Niklas Heer <me@nheer.io>
2025-01-21 09:18:33 -0800

    📝 Add better documentation for what is installed
    We don't need to be complicated

--- dot_pyinfra/mac.py
+++ dot_pyinfra/mac.py
@@ -1,8 +1,49 @@
 from pyinfra.operations import brew, server
 
-apps = ["zed", "arc", "ghostty", "1password", "1password-cli", "rocket", "raycast"]
+# Desktop apps
+apps = [
+    # My main editor
+    "zed",
+    # Main browser
+    "arc",
+    # My terminal
+    "ghostty",
+    # My password manager
+    "1password",
+    "1password-cli",
+    # My spotlight replacement and more
+    "raycast",
+    # This let's me get a lot of paid apps with one subscirption
+    "setapp",
+]
+
+# Cli apps
+clis = [
+    # My main shell
+    "nushell",
+    # For having emojis in git commit messages
+    "gitmoji",
+    # Great cli client for git
+    "lazygit",
+    # Great cli client for docker
+    "lazydocker",
+    # My tool to download git repos in a structured way
+    "ghq",
+    # Fuzzy finder I use to find everything
+    "fzf",
+    # My prompt theme engine for nu and zsh
+    "oh-my-posh",
+    # multi-shell completion library so nu is more useful
+    "carapace",
+    # replaces make for me with saner syntax
+    "just",
+    # replaces cat for me as it does syntax highlighting
+    "bat",
+]
+
+# Fonts I use
+# TODO: Add MonoLisa as my main font
 fonts = ["font-jetbrains-mono-nerd-font", "font-victor-mono-nerd-font"]
-clis = ["nushell", "gitmoji", "lazygit", "lazydocker", "ghq", "fzf", "oh-my-posh", "carapace"]
 
 def install_summary(desc, list):
     print(f"    Installing {desc}: {', '.join(list)}")
